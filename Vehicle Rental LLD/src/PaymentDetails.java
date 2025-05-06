120dc7b1c61412b6da1e1e27bcc664a7a5a410e5
aryan1234-coder <aryansachan2017@gmail.com>
2025-05-06 17:44:50 +0530

    Vehicle Rental Design Implementation

--- Vehicle Rental LLD/src/PaymentDetails.java
+++ Vehicle Rental LLD/src/PaymentDetails.java
@@ -2,13 +2,13 @@ import java.util.Date;
 
 public class PaymentDetails {
 
-
-
     int paymentID;
     int amountPaid;
     Date dateOfPayment;
     boolean isRefundable;
     PaymentMode paymentMode;
+
+
 
 
 
